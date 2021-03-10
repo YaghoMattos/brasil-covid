b875c00176e2255c743ac41c153d81d4322fdd6d
Yagho Mattos <mattosyagho@gmail.com>
2021-03-10 12:25:25 -0300

    int e cout hash.cpp

--- hash.cpp
+++ hash.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
 #include "hash.h"
-#include <sstream>
 
 using namespace std;
 
-Hash::Hash(int N){
-    this->numBaldes = N;
+Hash::Hash(int Nb){
+    this->numBaldes = Nb;
 }
 
 int Hash::hg(int k, int nivel){
-    long int result = 0;
-    cout << endl << "-----------------inicio----------------" << endl << "func hash k=" << k << " Balde=" << numBaldes << " nivel=" << nivel << " pow=" << (int)pow(2, nivel) << endl << endl;
+    int result = 0;
+    cout << endl << "-----------------inicio hg----------------" << endl << "func hash k=" << k << " Balde=" << numBaldes << " nivel=" << nivel << " pow=" << (int)pow(2, nivel) << endl << endl;
     result = k % (numBaldes * (int)pow(2, nivel));
     return result;
 }
 
 int Hash::hgMoreOne(int k, int nivel){
-    long int result = 0;
-    cout << endl << "-----------------inicio----------------" << endl << "func hash k " << k << " Baldes=" << numBaldes << " nivel=" << nivel << " pow=" << (int)pow(2, nivel) << endl << endl;
+    int result = 0;
+    cout << endl << "-----------------inicio hg+1----------------" << endl << "func hash k " << k << " Baldes=" << numBaldes << " nivel=" << nivel << " pow=" << (int)pow(2, nivel) << endl << endl;
     result = k % (numBaldes * (int)pow(2, (nivel + 1)));
     return result;
 }
