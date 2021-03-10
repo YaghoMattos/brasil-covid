30e3bd88d84d734b95ee1bf1777c113f898e8988
Yagho Mattos <mattosyagho@gmail.com>
2021-03-09 21:50:03 -0300

    inserção feita pela tabela e area de overflow

--- table.cpp
+++ table.cpp
@@ -1,40 +1,38 @@
 #include "table.h"
-#include "bucket.h"
-#include "hash.h"
+
 //#define M 5
 
 using namespace std;
 
-Table::Table(int N, int M) {
+Table::Table(int N, int Mb, int O) {
     g = 0;
     this->N = N;
-    this->M = M;
+    this->Mb = Mb;
+    this->O = O;
     chaves = 0;
-
-    Bucket* b1 = new Bucket();
-    Bucket* b2 = new Bucket();
-    Bucket* b3 = new Bucket();
-    Bucket* b4 = new Bucket();
-    Bucket* b5 = new Bucket();
-    tabela.push_back(b1);
-    tabela.push_back(b2);
-    tabela.push_back(b3);
-    tabela.push_back(b4);
-    tabela.push_back(b5);
-
-    numBaldes = 5;
     split = 0;
+    overflow = new int[O];
+
+    for(int i=0; i < O; i++) {
+        overflow[i] = -1;
+    }
+
+    for(int i = 0; i < N; i++) {
+        Bucket* balde = new Bucket(Mb);
+        tabela.push_back(balde);
+    }
+    Nb = N;
 }
 
 bool Table::verificaDivisao() {
-    //verifica se houve divisao no nivel g
-    if(g * N > numBaldes)
+    //verifica se houve divis�o no n�vel g
+    if(g * N > Nb)
         return true;
     return false;
 }
 
 void Table::ultimoBaldeNivelG() {
-    //verifica se split aponta para o ultimo balde do nivel g
+    //verifica se split aponta para o ultimo balde do n�vel g
     if( split == N - 1 || split == g * N - 1) {
         g++;
         split = (N * g) + 1; //reinicia split para o primeiro do nivel g
@@ -45,7 +43,7 @@ void Table::divideBalde() {
     //pega o balde apontado por split e divide as chaves com hgmoreone
     criaBalde();
     Hash haux(N);
-    for(int i = 0; i < M; i++) {
+    for(int i = 0; i < Mb; i++) {
         int chaveTroca;
         int index;
         chaveTroca = tabela.at(split)->getChave(i);
@@ -57,12 +55,54 @@ void Table::divideBalde() {
 }
 
 void Table::criaBalde() {
-    Bucket* novoBalde = new Bucket();
+    Bucket* novoBalde = new Bucket(Mb);
     tabela.push_back(novoBalde);
 }
 
-int Table::fatorCarga() {
-    return chaves / (numBaldes * M);
+double Table::fatorCarga() {
+    cout << endl << "chaves=" << chaves << " numBalde=" << Nb << "  M="  << Mb << endl;
+    return (float)chaves / (Nb * Mb);
+}
+
+void Table::insercao(int k1) {
+    Hash funcHash(N);
+    /*long long h = 0;
+    long long int k = 20200327120005;*/
+
+    int h = 0;
+    int k = k1;
+
+    h = funcHash.hg(k, getNivel());
+    cout << "h=" << h << " nivel=" << getNivel() << endl;
+    if(h < getSplit()) {
+        h = funcHash.hgMoreOne(k, getNivel());
+    }
+
+    if(tabela.at(h)->temEspaco()) {
+        cout << "tem espaco=" << tabela.at(h)->temEspaco() << endl;
+        //insere a chave na posi��o h
+        tabela.at(h)->insereChave(k);
+        chaves++;
+    }
+    else {
+        //verifica a ultima posi��o de overflow e se ela � nula tem espa�o
+        if(overflow[O-1] == -1) {
+            for(int i=0; i < O; i++) {
+                if(overflow[i] == -1) {
+                    overflow[i] = k;
+                }
+            }
+        }
+    }
+
+    cout << "fc=" << (float)chaves/(Nb*Mb+O) << endl;
+    if(fatorCarga() >= 0.8) {
+        divideBalde();
+    }
+    cout << "------------------fim------------------" << endl;
+}
+
+void imprimir() {
 }
 
 int Table::getNivel() {
@@ -73,11 +113,11 @@ void Table::setNivel(int nivel) {
 }
 
 int Table::getNumBaldes() {
-    return numBaldes;
+    return Nb;
 }
 
 void Table::setNumBaldes(int numBaldes) {
-    this->numBaldes = numBaldes;
+    this->Nb = numBaldes;
 }
 
 int Table::getSplit() {
--- table.h
+++ table.h
@@ -1,38 +1,44 @@
-#include "bucket.h"
+#include <iostream>
 #include <vector>
+#include "hash.h"
+#include "bucket.h"
 
 using namespace std;
 
 //armazena e manipula os baldes
 class Table {
     private:
-        //nivel da tabela
+        //n�vel da tabela
         int g;
         //numero de chaves por balde
-        int M;
-        //numero de baldes por n�vel
+        int Mb;
+        //n�mero de baldes por n�vel
         int N;
+        //posi��es no overflow
+        int O;
         //numero total de baldes
-        int numBaldes;
+        int Nb;
         //numero de chaves inseridas
         int chaves;
         //ponteiro para indicar o balde
         int split;
-
+        int *overflow;
 
     public:
+        Table(int N, int M, int O);
         //lista de baldes
         vector<Bucket*> tabela;
-        Table(int N, int M);
         bool verificaDivisao();
         void ultimoBaldeNivelG();
         void divideBalde();
         void criaBalde();
-        int fatorCarga();
+        double fatorCarga();
         int getNivel();
         void setNivel(int nivel);
         int getNumBaldes();
-        void setNumBaldes(int numBaldes);
+        void setNumBaldes(int Nb);
         void setSplit(int split);
         int getSplit();
+        void insercao(int k1);
+        void imprimir();
 };
