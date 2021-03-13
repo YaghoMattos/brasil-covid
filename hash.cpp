94f81d17e1ed6f98fa59b01f8bc321c050fcb8a2
Yagho Mattos <mattosyagho@gmail.com>
2021-03-13 00:47:48 -0300

    N diferente de Nb

--- hash.cpp
+++ hash.cpp
@@ -3,20 +3,20 @@
 
 using namespace std;
 
-Hash::Hash(int Nb){
-    this->numBaldes = Nb;
+Hash::Hash(int N){
+    this->numBaldes = N;
 }
 
 int Hash::hg(int k, int nivel){
     int result = 0;
-    cout << endl << "|||||||| hash por hg ||||||||" << endl << "func hash k = " << k << " baldes = " << numBaldes << " nivel = " << nivel << " pow = " << (int)pow(2, nivel) << endl << endl;
+    cout << endl << "|||||||| hash por hg ||||||||" << endl << "func hash k = " << k << ", baldes = " << numBaldes << ", nivel = " << nivel << ", pow = " << (int)pow(2, nivel) << ", k % " << numBaldes * (int)pow(2, (nivel)) << endl << endl;
     result = k % (numBaldes * (int)pow(2, nivel));
     return result;
 }
 
 int Hash::hgMoreOne(int k, int nivel){
     int result = 0;
-    cout << endl << "|||||||| hash por hg+1 ||||||||" << endl << "func hash k " << k << " baldes= " << numBaldes << " nivel = " << nivel << " pow = " << (int)pow(2, nivel) << endl << endl;
+    cout << endl << "|||||||| hash por hg+1 ||||||||" << endl << "func hash k = " << k << ", baldes = " << numBaldes << ", nivel = " << nivel << ", pow = " << (int)pow(2, nivel) << ", k % " << numBaldes * (int)pow(2, (nivel + 1)) << endl << endl;
     result = k % (numBaldes * (int)pow(2, (nivel + 1)));
     cout << "result hg+1 = " << k % (numBaldes * (int)pow(2, (nivel + 1))) << endl;
     return result;
